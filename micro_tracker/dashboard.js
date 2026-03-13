90c4c23c76b478084de912b3b1177987d6aa9b48
SpeedOverload <bruggeman.raphael@hotmail.com>
2026-03-13 13:28:54 +0100

    Second level terminated

--- micro_tracker/dashboard.js
+++ micro_tracker/dashboard.js
@@ -3,25 +3,49 @@ let vWater = 0;
 let vBreak = 0;
 
 let btn_sun = document.getElementById("btn-sun");
+let btn_sun_5 = document.getElementById("btn-sun-5");   
 let btn_water = document.getElementById("btn-water");
+let btn_water_5 = document.getElementById("btn-water-5");
 let btn_break = document.getElementById("btn-break");
+let btn_break_5 = document.getElementById("btn-break-5");
 
 let sun_value = document.getElementById("sun-value");
 let water_value = document.getElementById("water-value");
 let break_value = document.getElementById("break-value");
 
 btn_sun.addEventListener("click", () => {
-    sun_value.innerText = parseInt(sun_value.innerText) + 1;
+    ++vSun;
+    sun_value.innerText = vSun;
+    console.log(sun_value.innerText);
+});
+
+btn_sun_5.addEventListener("click", () => {
+    vSun += 5;
+    sun_value.innerText = vSun;
     console.log(sun_value.innerText);
 });
 
 btn_water.addEventListener("click", () => {
-    water_value.innerText = parseInt(water_value.innerText) + 1;
+    ++vWater;
+    water_value.innerText = vWater;
+    console.log(water_value.innerText);
+});
+
+btn_water_5.addEventListener("click", () => {
+    vWater += 5;
+    water_value.innerText = vWater;
     console.log(water_value.innerText);
 });
 
 btn_break.addEventListener("click", () => {
-    break_value.innerText = parseInt(break_value.innerText) + 1;
+    ++vBreak;
+    break_value.innerText = vBreak;
+    console.log(break_value.innerText);
+});
+
+btn_break_5.addEventListener("click", () => {
+    vBreak += 5;
+    break_value.innerText = vBreak;
     console.log(break_value.innerText);
 });
 
