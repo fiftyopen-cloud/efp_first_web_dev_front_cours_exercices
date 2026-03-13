351ba8e94fb9a68a339245bb15f992955dee7c66
SpeedOverload <bruggeman.raphael@hotmail.com>
2026-03-13 15:07:54 +0100

    the top final level closed

--- micro_tracker/dashboard.js
+++ micro_tracker/dashboard.js
@@ -2,6 +2,10 @@ let vSun = 0;
 let vWater = 0;
 let vBreak = 0;
 
+let vSun_limit = 6; 
+let vWater_limit = 6;
+let vBreak_limit = 6;
+
 let btn_sun = document.getElementById("btn-sun");
 let btn_sun_5 = document.getElementById("btn-sun-5");   
 let btn_water = document.getElementById("btn-water");
@@ -16,51 +20,86 @@ let break_value = document.getElementById("break-value");
 let total_value = document.getElementById("total-value");
 
 btn_sun.addEventListener("click", () => {
-    ++vSun;
-    sun_value.innerText = vSun;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(sun_value.innerText);
+    if (vSun < vSun_limit) {
+        ++vSun;
+        sun_value.innerText = vSun;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(sun_value.innerText);
+    } 
+    
+    btn_sun.disabled = (vSun+1 > vSun_limit);
+    btn_sun_5.disabled = (vSun+5 > vSun_limit);
+
 });
 
 btn_sun_5.addEventListener("click", () => {
-    vSun += 5;
-    sun_value.innerText = vSun;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(sun_value.innerText);
+    if (vSun + 5 <= vSun_limit) {
+        vSun += 5;
+        sun_value.innerText = vSun;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(sun_value.innerText);
+    }
+
+    btn_sun.disabled = (vSun+1 > vSun_limit);
+    btn_sun_5.disabled = (vSun+5 > vSun_limit);
+    
 });
 
 btn_water.addEventListener("click", () => {
-    ++vWater;
-    water_value.innerText = vWater;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(water_value.innerText);
+    if (vWater + 1 <= vWater_limit) {
+        ++vWater;
+        water_value.innerText = vWater;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(water_value.innerText);
+    }
+
+    btn_water.disabled = (vWater+1 > vWater_limit);
+    btn_water_5.disabled = (vWater+5 > vWater_limit);
+
 });
 
 btn_water_5.addEventListener("click", () => {
-    vWater += 5;
-    water_value.innerText = vWater;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(water_value.innerText);
+    if (vWater + 5 <= vWater_limit) {
+        vWater += 5;
+        water_value.innerText = vWater;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(water_value.innerText);
+    }
+
+    btn_water.disabled = (vWater+1 > vWater_limit);
+    btn_water_5.disabled = (vWater+5 > vWater_limit);
+
 });
 
 btn_break.addEventListener("click", () => {
-    ++vBreak;
-    break_value.innerText = vBreak;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(break_value.innerText);
+    if (vBreak < vBreak_limit) {
+        ++vBreak;
+        break_value.innerText = vBreak;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(break_value.innerText);
+    }
+
+    btn_break.disabled = (vBreak+1 > vBreak_limit);
+    btn_break_5.disabled = (vBreak+5 > vBreak_limit);
+
 });
 
 btn_break_5.addEventListener("click", () => {
-    vBreak += 5;
-    break_value.innerText = vBreak;
-    total_value.innerText = vSun + vWater + vBreak;
-    console.log(total_value.innerText);
-    console.log(break_value.innerText);
+    if (vBreak + 5 <= vBreak_limit) {
+        vBreak += 5;
+        break_value.innerText = vBreak;
+        total_value.innerText = vSun + vWater + vBreak;
+        console.log(total_value.innerText);
+        console.log(break_value.innerText);
+    }
+
+    btn_break.disabled = (vBreak+1 > vBreak_limit);
+    btn_break_5.disabled = (vBreak+5 > vBreak_limit);
 });
 
 btn_reset.addEventListener("click", () => {
@@ -74,6 +113,14 @@ btn_reset.addEventListener("click", () => {
 
     total_value.innerText = 0;
     
+    btn_sun.disabled = false;
+    btn_sun_5.disabled = false;
+    btn_water.disabled = false;
+    btn_water_5.disabled = false;
+    btn_break.disabled = false;
+    btn_break_5.disabled = false;
+
+
     console.log(sun_value.innerText);
     console.log(water_value.innerText);
     console.log(break_value.innerText);
